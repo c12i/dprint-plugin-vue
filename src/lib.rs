a652ba08b8be162fe18e7d4b8a31006ceb094dd7
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-06-13 12:07:40 +0300

    expose format function to public api

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod configuration;
 mod format;
 mod plugin;
 
+pub use format::format;
+
 #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
 mod wasm {
     use crate::{configuration::Configuration, plugin::VuePluginHandler};
